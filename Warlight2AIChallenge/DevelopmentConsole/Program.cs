dd68215c6a5480e46c6b7690df02fb2716c6ce3b
RAGNOARAKNOS <ragnocorp@googlemail.com>
2015-05-20 21:47:42 +0100

    InitialCommit

---
+++ Warlight2AIChallenge/DevelopmentConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Program.cs" company="RAGNOARAKNOS">
+//  This codebase and associated tools are covered by the MIT license
+// </copyright>
+namespace Warlight2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The main entry point for my development console
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main method entry point for me development console
+        /// </summary>
+        /// <param name="args">Command line parameters</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
