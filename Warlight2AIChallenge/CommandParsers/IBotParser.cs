1f55089d16dbfebc3d8981ee6b20b5b88b1c43f1
RAGNOARAKNOS <ragnocorp@googlemail.com>
2015-05-20 23:26:32 +0100

    Added command parser library

---
+++ Warlight2AIChallenge/CommandParsers/IBotParser.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IBotParser.cs" company="RAGNOARAKNOS">
+//  This codebase and associated tools are covered by the MIT license
+// </copyright>
+namespace Warlight2.CommandParsers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines what interface contract any bot parser must adhere to
+    /// </summary>
+    public interface IBotParser
+    {
+    }
+}
